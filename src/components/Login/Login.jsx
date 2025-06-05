980865b17e78b37de4ebf7af7c4c304648a977e6
mdmhrz <razufreelance@gmail.com>
2025-06-05 23:10:30 +0600

    Sign in with google done using firebase authenticaiton

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,9 +1,20 @@
+import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 import React from 'react';
+import { auth } from '../../firebase/firebase.init';
 
 const Login = () => {
+    const provider = new GoogleAuthProvider();
+
     const handleGoogleSignIn = () => {
         console.log('Sign In button clicked');
+        signInWithPopup(auth, provider)
+            .then(res => {
+                console.log(res);
+            }).catch(error => {
+                console.log(error);
+            })
     }
+
 
     return (
         <div>
