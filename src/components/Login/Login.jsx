d93cf3ab60c4660f9438be2430b188b0d99f2ab5
mdmhrz <razufreelance@gmail.com>
2025-06-06 02:05:40 +0600

    Login in and logout and load google data using firebase

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,25 +1,48 @@
-import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
-import React from 'react';
+import { GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
+import React, { useState } from 'react';
 import { auth } from '../../firebase/firebase.init';
 
 const Login = () => {
+
+    const [user, setUser] = useState(null)
     const provider = new GoogleAuthProvider();
 
     const handleGoogleSignIn = () => {
         console.log('Sign In button clicked');
         signInWithPopup(auth, provider)
             .then(res => {
-                console.log(res);
+                console.log(res.user);
+                setUser(res.user)
+
             }).catch(error => {
                 console.log(error);
             })
     }
 
+    const handleSignOut = () => {
+        signOut(auth).then(() => {
+            console.log("sign out completed");
+            setUser(null)
+        }).catch(error => {
+            console.log(error);
+        })
+    }
 
     return (
         <div>
             <h2>Please Log in</h2>
-            <button onClick={handleGoogleSignIn}>Sign In with Google</button>
+            {
+                user ? <button onClick={handleSignOut}>Sign Out</button> : <button onClick={handleGoogleSignIn}>Sign In with Google</button>
+            }
+
+
+            {
+                user && <div>
+                    <h4>{user.displayName}</h4>
+                    <p>{user.email}</p>
+                    <img src={user.photoURL} alt="" />
+                </div>
+            }
         </div>
     );
 };
